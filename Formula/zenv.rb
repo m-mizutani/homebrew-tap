a826741c75cedf1714ecc00660c799a801af2d32
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2026-06-14 08:45:55 +0900

    Use prebuilt release binaries for zenv

--- Formula/zenv.rb
+++ Formula/zenv.rb
@@ -1,15 +1,33 @@
 class Zenv < Formula
   desc "Enhanced env command to manage environment variables in CLI"
   homepage "https://github.com/m-mizutani/zenv"
-  url "https://github.com/m-mizutani/zenv/archive/refs/tags/v2.5.0.tar.gz"
-  sha256 "679b631cf01cce18a03d10432022a514bc05789f7f74dc29def42395a2140bae"
+  version "2.5.0"
   license "Apache-2.0"
-  head "https://github.com/m-mizutani/zenv.git", branch: "main"
 
-  depends_on "go" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-darwin-arm64.tar.gz"
+      sha256 "227b0d6f3a1f11a72d60ab90013f38f501897c3255377a914a118953e37389a9"
+    end
+    on_intel do
+      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-darwin-amd64.tar.gz"
+      sha256 "3363be33e08f5a598d62c36194345218e0d2e5608d50826565ff5b1ed0adf60d"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-linux-arm64.tar.gz"
+      sha256 "ca766ea1d9d41426563b86c7aeab39bcbf5d772a57823fb7a093375f0ceb2278"
+    end
+    on_intel do
+      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-linux-amd64.tar.gz"
+      sha256 "b316d86a78dc8dce721cb0874634ffa48e3feafea0f6e9dddccb7d89d0c8e752"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+    bin.install "zenv"
   end
 
   test do
