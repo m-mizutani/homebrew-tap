a1b49db6a0ab82816653a1fe113b453155d7b531
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2026-08-19 07:44:58 +0900

    fix: drop redundant version field from formulae to satisfy brew audit

--- Formula/goast.rb
+++ Formula/goast.rb
@@ -1,7 +1,6 @@
 class Goast < Formula
   desc "Static analysis tool for Go source AST with Rego"
   homepage "https://github.com/m-mizutani/goast"
-  version "0.3.0"
   license "Apache-2.0"
 
   livecheck do
@@ -11,22 +10,22 @@ class Goast < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/m-mizutani/goast/releases/download/v#{version}/goast_darwin_arm64.tar.gz"
+      url "https://github.com/m-mizutani/goast/releases/download/v0.3.0/goast_darwin_arm64.tar.gz"
       sha256 "af1067d495b4379339614a8f27a2c906064e08db4ec7590dfd0003513bad9219"
     end
     on_intel do
-      url "https://github.com/m-mizutani/goast/releases/download/v#{version}/goast_darwin_amd64.tar.gz"
+      url "https://github.com/m-mizutani/goast/releases/download/v0.3.0/goast_darwin_amd64.tar.gz"
       sha256 "b061564f83e32e1f74e4185abd596178d37106f329fe7d10a587661e97a6f741"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/m-mizutani/goast/releases/download/v#{version}/goast_linux_arm64.tar.gz"
+      url "https://github.com/m-mizutani/goast/releases/download/v0.3.0/goast_linux_arm64.tar.gz"
       sha256 "eb4f2f3811a3f7049bbaea1620d57015c88e112221b0badbe517cca8c2390deb"
     end
     on_intel do
-      url "https://github.com/m-mizutani/goast/releases/download/v#{version}/goast_linux_amd64.tar.gz"
+      url "https://github.com/m-mizutani/goast/releases/download/v0.3.0/goast_linux_amd64.tar.gz"
       sha256 "341f55a0e15e65b761830ae74ecc48278aaed9a32562fc9ef47248d24bfb483f"
     end
   end
--- Formula/zenv.rb
+++ Formula/zenv.rb
@@ -1,7 +1,6 @@
 class Zenv < Formula
   desc "Enhanced env command to manage environment variables in CLI"
   homepage "https://github.com/m-mizutani/zenv"
-  version "2.5.1"
   license "Apache-2.0"
 
   livecheck do
@@ -11,22 +10,22 @@ class Zenv < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-darwin-arm64.tar.gz"
+      url "https://github.com/m-mizutani/zenv/releases/download/v2.5.1/zenv-v2.5.1-darwin-arm64.tar.gz"
       sha256 "d747605541f33305d942a0824a1222351004c4a9ec94893b59178999d7db8608"
     end
     on_intel do
-      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-darwin-amd64.tar.gz"
+      url "https://github.com/m-mizutani/zenv/releases/download/v2.5.1/zenv-v2.5.1-darwin-amd64.tar.gz"
       sha256 "cf97b38daa0abcf3dae70c8568b996e50f8182ed6f9023d8d993991ae608e74d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-linux-arm64.tar.gz"
+      url "https://github.com/m-mizutani/zenv/releases/download/v2.5.1/zenv-v2.5.1-linux-arm64.tar.gz"
       sha256 "fef2b4f5862e9b1426f91d95c5907b1cefe6e29895473c2869d22687fed16373"
     end
     on_intel do
-      url "https://github.com/m-mizutani/zenv/releases/download/v#{version}/zenv-v#{version}-linux-amd64.tar.gz"
+      url "https://github.com/m-mizutani/zenv/releases/download/v2.5.1/zenv-v2.5.1-linux-amd64.tar.gz"
       sha256 "a927edbcb24179860e995c1615fcb26c64dfc925c6d69b70c6c9edc945665319"
     end
   end
